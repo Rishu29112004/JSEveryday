3286772fc0ddad798a033b727c0a6c5be4230fd8
mani <kumarrishuritik1@gmail.com>
2025-11-18 06:06:39 +0530

    practice

--- JS-Interview-Qt/day1.js
+++ JS-Interview-Qt/day1.js
@@ -1,12 +1,65 @@
-//1.Is javascript a dynamically typed language or a statically typed language ?
-//JavaScript is a dynamically typed language.
-let a
-a=0
-console.log(a)
-a="hello"
-console.log(a)
+// //1.Is javascript a dynamically typed language or a statically typed language ?
+// //JavaScript is a dynamically typed language.
+// let a
+// a=0
+// console.log(a)
+// a="hello"
+// console.log(a)
 
-//TypeScript is statically typed.
-// let b: number=5;
-// x="hello";   //Error at compile time
+// //TypeScript is statically typed.
+// // let b: number=5;
+// // x="hello";   //Error at compile time
 
+
+
+// //🔥 5 Hoisting Trick Questions (With Answers)
+// //1
+// console.log(a);
+// var a = 10;
+
+// //2
+// test();
+// function test() {
+//   console.log("Hello");
+// }
+
+// //3.
+// greet();
+// var greet = function () {
+//   console.log("Hi");
+// };
+
+// //4.
+// console.log(typeof x);
+// var x = 20;
+
+// //5.
+// console.log(foo);
+// function foo() {}
+// var foo = 20;
+
+
+// //⚡ 5 TDZ Coding Traps (with explanations)
+// //1.
+// console.log(a);
+// let a = 10;
+
+// //2.
+// {
+//   console.log(b);
+//   let b = 50;
+// }
+
+// //3.
+// console.log(typeof x);
+// let x = 5;
+
+// //4.console.log(p);
+// const p = 99;
+
+
+// //5.
+// {
+//   console.log(fun);
+//   function fun() {}
+// }
