ea10e5b95e2cbc337b9a9414867b4cbde15a2828
rishu29112004 <kumarrishuritik1@gmail.com>
2026-07-28 01:52:27 +0530

    sadf

---  Topics/MyMap.js
+++  Topics/MyMap.js
@@ -12,4 +12,19 @@ function cb(value){
     return value*10
 }
 
-console.log(arr.myMap(cb))
+// console.log(arr.myMap(cb))
+
+
+Array.prototype.myMappp=function(cbb){
+    const newArray=[]
+    for(let i =0; i<this.length; i++){
+        newArray.push(cbb(this[i]))
+    }
+    return newArray
+}
+
+function cbb(value){
+    return value*58
+}
+
+console.log(arr.myMappp(cbb))
