61dee698aeb2521e211f0038e79c6fe536b88c5f
rishu29112004 <kumarrishuritik1@gmail.com>
2026-07-30 01:05:16 +0530

    asdf

---
+++  Topics/Promises/second.js
@@ -0,0 +1,32 @@
+// async function fetchData(){
+//     try{
+//         const response=await fetch("https:/dummyjs.com/users")
+
+//         if(!response.ok){
+//             throw new Error("Failed to fetch Data")
+//         }
+
+//             const data=await response.json();
+//             console.log("Data", data)
+//     }
+//     catch(error){
+//         console.log("Error", error.message)
+//     }
+// }
+
+// fetchData()
+
+
+async function fetchData() {
+    try{
+        const response= await fetch("https://dummyjson.com/users")
+
+        const data=await response.json()
+        console.log(data)
+    }
+    catch(error){
+      console.log(error)
+    }
+}
+
+fetchData()
