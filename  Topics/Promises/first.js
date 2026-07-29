01c7b6045c53472eb14020a4784e639bf662abfc
rishu29112004 <kumarrishuritik1@gmail.com>
2026-07-30 00:40:00 +0530

    yoyo

---  Topics/Promises/first.js
+++  Topics/Promises/first.js
@@ -1,12 +1,26 @@
-const promise1= ()=>{
-    return new Promise((res,rej)=>{
+const promise1=()=>{
+   return new Promise((res,rej)=>{
         setTimeout(()=>{
-            const user={name: "rishu", email: "rishu@gmail.com"}
-               res(user)
+            const user={name:"Rishu", class:"C"}
+            res(user)
         },1000)
     })
 }
 
-promise1().then((data)=>{
-    console.log(data)
+const promise2=(user)=>{
+   return new Promise((res,rej)=>{
+        setTimeout(()=>{
+           const name=user?.name
+           res(name)
+        },1000)
+    })
+}
+
+promise1()
+.then((user)=>promise2(user))
+.then((name)=>{
+    console.log(name)
+})
+.catch((err)=>{
+    console.log(error)
 })
