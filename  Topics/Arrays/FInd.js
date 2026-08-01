0fe56801c203612ae67bc650cfce80947c56e56a
rishu29112004 <kumarrishuritik1@gmail.com>
2026-08-02 00:14:07 +0530

    find

---
+++  Topics/Arrays/FInd.js
@@ -0,0 +1,51 @@
+// Definition
+// find() is used to return the first element in an array that satisfies a given condition.
+
+
+// 1. Find First Adult User
+// const users = [
+//   { name: "Rahul", age: 16 },
+//   { name: "Rishu", age: 22 },
+//   { name: "Aman", age: 25 }
+// ];
+// const res=users.find((t)=>t.age>=18)
+// console.log(res)
+
+
+// 2. Find Product by ID
+// 👉 Find the product whose id is 2.
+// const products = [
+//   { id: 1, name: "Laptop" },
+//   { id: 2, name: "Mouse" },
+//   { id: 3, name: "Keyboard" }
+// ];
+
+// const res= products.find((t)=>t.id==2)
+// console.log(res)
+
+
+// 3. Find First Even Number
+// const numbers = [11, 15, 21, 24, 30, 42];
+// const res= numbers.find((t)=>t%2==0)
+// console.log(res)
+
+// 4. Find Student with Marks Greater Than 80
+// const students = [
+//   { name: "Priya", marks: 72 },
+//   { name: "Aman", marks: 85 },
+//   { name: "Rahul", marks: 91 }
+// ];
+
+// const res= students.find((t)=>t.marks>=80)
+// console.log(res)
+
+
+// 5. Find First Expensive Product
+// const products = [
+//   { name: "Pen", price: 20 },
+//   { name: "Book", price: 150 },
+//   { name: "Laptop", price: 50000 }
+// ];
+
+// const res=products.find((t)=>t.price>=1000)
+// console.log(res)
