3aef60095df49bb56fb13a84a058f5c5f3d9692b
rishu29112004 <kumarrishuritik1@gmail.com>
2026-08-02 00:35:39 +0530

    dsf

---
+++  Topics/Arrays/FindIndex.js
@@ -0,0 +1,57 @@
+// Definition
+// findIndex() is used to return the index of the first element that satisfies a given condition.
+
+
+// 1. Find Index of First Adult User
+// const users = [
+//   { name: "Rahul", age: 16 },
+//   { name: "Rishu", age: 22 },
+//   { name: "Aman", age: 25 }
+// ];
+
+// const res= users.findIndex((t)=> t.age>=18)
+// console.log(res)
+
+
+
+
+// 2. Find Index of Product with ID 3
+// const products = [
+//   { id: 1, name: "Laptop" },
+//   { id: 2, name: "Mouse" },
+//   { id: 3, name: "Keyboard" }
+// ];
+
+// const res= products.findIndex((t)=>t.id===3)
+// console.log(res)
+
+
+// 3. Find Index of First Even Number
+// const numbers = [11, 15, 21, 24, 30, 42];
+
+// const res= numbers.findIndex((t)=>t%2==0)
+// console.log(res)
+
+
+// // 4. Find Index of Student Scoring Above 90
+// const students = [
+//   { name: "Priya", marks: 72 },
+//   { name: "Aman", marks: 85 },
+//   { name: "Rahul", marks: 91 }
+// ];
+
+// const res= students.findIndex((t)=>t.marks>=90)
+// console.log(res)
+
+
+// 5. Find Index of First Expensive Product
+// const products = [
+//   { name: "Pen", price: 20 },
+//   { name: "Book", price: 150 },
+//   { name: "Laptop", price: 50000 }
+// ];
+
+// const res=products.findIndex((t)=>t.price>=1000)
+// console.log(res)
+
+
