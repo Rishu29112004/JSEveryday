351e040a902c079211cca2a14b61857a4815771c
rishu29112004 <kumarrishuritik1@gmail.com>
2026-08-21 21:17:00 +0530

    asdfg

---  Topics/Arrays/Every.js
+++  Topics/Arrays/Every.js
@@ -54,7 +54,7 @@
 
 
 
-// // 5. Check if all products are in stock
+// 5. Check if all products are in stock
 // const products = [
 //   { name: "Laptop", inStock: true },
 //   { name: "Mouse", inStock: true },
@@ -66,3 +66,36 @@
 
 // const res=products.every((t)=>t.inStock===true)
 // console.log(res)
+
+// Q6. Check whether all numbers are positive.
+
+// let arr = [10, 20, 30, 40, 50];
+
+// let arrAns=arr.every((t)=>t>0)
+// console.log(arrAns)
+
+
+// Q7.Check whether all numbers are even.
+// let arr = [2, 4, 6, 8, 10];
+// let arrRes=arr.every((t)=>t%2==0)
+// console.log(arrRes)
+
+
+
+// Q8. Check whether all students have passed (marks ≥ 40).
+// let marks = [55, 67, 42, 80, 39];
+// let marksRes=marks.every((t)=>t>=40)
+// console.log(marksRes)
+
+
+// Q9. Check whether all numbers are greater than 10.
+
+// let arr = [15, 20, 25, 30, 5];
+// const arrRes=arr.every((t)=>t>10)
+// console.log(arrRes)
+
+// Q10. Check whether all names have more than 3 characters.
+
+// let names = ["Rishu", "Aman", "Rahul", "Raj"];
+// let namesRes=names.every((t)=>t.length>3)
+// console.log(namesRes)
