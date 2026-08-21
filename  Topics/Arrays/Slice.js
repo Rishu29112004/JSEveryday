2acc9ef3cbbf54ad5311e54200818bc0a9197dfd
rishu29112004 <kumarrishuritik1@gmail.com>
2026-08-21 21:29:29 +0530

    adsf

---
+++  Topics/Arrays/Slice.js
@@ -0,0 +1,34 @@
+// slice() is an array method used to extract a portion of an array without changing the original array.
+
+// Q1. Get the first 3 elements
+
+// let arr = [10, 20, 30, 40, 50];
+// let arrRes= arr.slice(0,3)
+// console.log(arrRes)
+
+
+// Q2. Get the last 3 elements
+
+// let arr = [10, 20, 30, 40, 50];
+// const arrRes=arr.slice(2)
+// console.log(arrRes)
+
+
+// Q3. Get elements from index 1 to index 4
+
+// let arr = ["A", "B", "C", "D", "E"];
+// let arrRes=arr.slice(1,4)
+// console.log(arrRes)
+
+// Q4. Copy the entire array using slice()
+
+// let arr = [5, 10, 15, 20, 25];
+// const arrRes=arr.slice()
+// console.log(arrRes)
+
+
+// Q5. Get "React" and "Node"
+
+// let skills = ["HTML", "CSS", "JavaScript", "React", "Node", "MongoDB"];
+// const skillsRes=skills.slice(3,5)
+// console.log(skillsRes)
