732ea56f91a289fb55421f50d692f18d6df0bc2d
rishu29112004 <kumarrishuritik1@gmail.com>
2026-08-22 00:43:59 +0530

    afsfg

---
+++  Topics/Arrays/Splice.js
@@ -0,0 +1,35 @@
+// splice() is an array method used to add, remove, or replace elements in an array.
+
+// Q1. Remove 2 elements
+// let arr = [10, 20, 30, 40, 50];
+
+// let arrRes=arr.splice(1,2)
+// console.log(arrRes)
+
+
+// Q2. Add an element
+// let arr = [10, 20, 40, 50];
+// // Add 30 at index 2
+// let arrRes=arr.splice(2,0,30)
+// console.log(arr)
+
+
+// Q3. Remove 1 element and add 2 elements
+// let arr = [10, 20, 30, 40];
+// // Remove 20 and add 25, 27
+// const arrRes= arr.splice(1,1,25,27)
+// console.log(arr)
+
+
+// Q4. Remove the first element
+// let arr = [10, 20, 30, 40];
+// // Remove the first element
+// const arrRes=arr.splice(0,1)
+// console.log(arr)
+
+
+// // Q5. Replace an element
+// let arr = ["HTML", "CSS", "Java", "Python"];
+// // Replace "Java" with "JavaScript"
+// const arrRes=arr.splice(2,1,"JavaScript")
+// console.log(arr)
